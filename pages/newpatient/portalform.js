f9a7cae2738905fb27a0db277a183e1d87a710be
Dylan <dylan.eye@outlook.com>
2019-04-14 18:14:24 -0500

    Week 5
    UI is complete.. Data/Services begin

--- pages/newpatient/portalform.js
+++ pages/newpatient/portalform.js
@@ -101,19 +101,15 @@ $(document).ready(function() {
           }
       })
       .on('success.form.bv', function(e) {
-          $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+          $('#success_message').slideDown({ opacity: "show" }, "slow")
               $('#contact_form').data('bootstrapValidator').resetForm();
 
-          // Prevent form submission
           e.preventDefault();
 
-          // Get the form instance
           var $form = $(e.target);
 
-          // Get the BootstrapValidator instance
           var bv = $form.data('bootstrapValidator');
 
-          // Use Ajax to submit form data
           $.post($form.attr('action'), $form.serialize(), function(result) {
               console.log(result);
           }, 'json');
